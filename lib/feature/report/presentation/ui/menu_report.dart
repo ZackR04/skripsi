cb516d5be0cb9854ce859caa1daec09453578b87
fanforfan <fanforfan26@gmail.com>
2022-03-20 18:13:28 +0700

    form add report

--- lib/feature/report/presentation/ui/menu_report.dart
+++ lib/feature/report/presentation/ui/menu_report.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skripsi_residencereport/feature/report/presentation/ui/sub_add_report/add_report_screen.dart';
 import 'package:skripsi_residencereport/feature/report/presentation/ui/sub_my_report/my_report_screen.dart';
 import 'sub_report/list_report.dart';
 import 'package:steps_indicator/steps_indicator.dart';
@@ -102,7 +103,7 @@ class _MenuReportScreenState extends State<MenuReportScreen> {
                             child: FloatingActionButton(
                               child: Icon(Icons.add),
                               onPressed: (){
-                                // Navigator.push(context, MaterialPageRoute(builder: (context) => FormAddReportScreen()));
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => AddReportScreen()));
                               },
                             ),
                           ),
